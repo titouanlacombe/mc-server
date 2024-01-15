ed5259622d56825e5c8eeac97ce81f47e4d98adc
Titouan Lacombe <titouan.lacombe99@gmail.com>
2024-01-15 23:30:08 +0100

    udp tunnel prototype working

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -2,3 +2,10 @@
 
 # TCP tunnel for mincraft server
 exec ssh -nNT -R 25565:localhost:25565 smoll
+
+# UDP tunnel:
+# On the server:
+# socat UDP-LISTEN:udp_port,fork TCP:localhost:tcp_port
+# On the client:
+# ssh -nNT -R tcp_port:localhost:tcp_port smoll
+# socat TCP-LISTEN:tcp_port,fork UDP:localhost:udp_port
