403e2ab54c9886c2d517704ff5d0cd0e1c46ece7
Titouan Lacombe <titouan.lacombe99@gmail.com>
2025-04-11 21:12:26 +0200

    feat: add tunnel server cleanup script

--- tunnel-server-cleanup.sh
+++ tunnel-server-cleanup.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-# TODO
+# Remove forwarding rules
+sudo iptables -D FORWARD -i $TUNNEL_INTERFACE_PUB -o $TUNNEL_INTERFACE_VPN -j ACCEPT
+sudo iptables -D FORWARD -i $TUNNEL_INTERFACE_VPN -o $TUNNEL_INTERFACE_PUB -j ACCEPT
+
+# Remove NAT rules for translating packets
+sudo iptables -t nat -D PREROUTING -p tcp -m tcp --dport $MC_PORT -i $TUNNEL_INTERFACE_PUB -j DNAT --to-destination $TUNNEL_LOCAL_IP:$MC_PORT
+sudo iptables -t nat -D PREROUTING -p udp -m udp --dport $VOICE_PORT -i $TUNNEL_INTERFACE_PUB -j DNAT --to-destination $TUNNEL_LOCAL_IP:$VOICE_PORT
+
+# Remove reverse NAT rules
+sudo iptables -t nat -D POSTROUTING -p tcp -m tcp --dport $MC_PORT -d $TUNNEL_LOCAL_IP -j SNAT --to-source $TUNNEL_VPN_IP
+sudo iptables -t nat -D POSTROUTING -p udp -m udp --dport $VOICE_PORT -d $TUNNEL_LOCAL_IP -j SNAT --to-source $TUNNEL_VPN_IP
