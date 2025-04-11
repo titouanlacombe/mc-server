c837086f16b014e22760014569b92febb1c6145f
Titouan Lacombe <titouan.lacombe99@gmail.com>
2025-04-11 21:39:31 +0200

    client tunnel script working

--- tunnel.sh
+++ tunnel.sh
@@ -3,12 +3,13 @@
 # Function to execute the tunnel script on the remote server
 manage_tunnel() {
     local action=$1
-    ACTION=$action envsubst < ./tunnel-server.sh | ssh $TUNNEL_SSH_HOST "sudo bash -s"
+    ACTION=$action envsubst < ./tunnel-server.sh
+    # ACTION=$action envsubst < ./tunnel-server.sh | ssh $TUNNEL_SSH_HOST "sudo bash -s"
 }
 
 # Clean up function
 cleanup() {
-    echo "Cleaning up..."
+    echo -e "\nCleaning up..."
     manage_tunnel "-D"
     exit 0
 }
@@ -19,4 +20,7 @@ trap cleanup INT
 echo "Launching tunnel script on the server..."
 manage_tunnel "-A"
 
-wait
+echo -e "\nTunnel is running. Press Ctrl+C to exit."
+while true; do
+    sleep 1
+done
